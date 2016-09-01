c68ee726ed047528f8b836d20e95a4ef7c0d0499
ChachaNy <nancy.zappellaz@students.hevs.ch>
2016-09-01 15:00:13 +0200

    Transports auxiliaires (vues, logique à implémenter -> commencé)

---
+++ app/src/main/java/fiesta/hevs/ch/myapplication/TransportAltActivity.java
@@ -0,0 +1,7 @@
+package fiesta.hevs.ch.myapplication;
+
+/**
+ * Created by Chacha on 01.09.2016.
+ */
+public class TransportAltActivity {
+}
---
+++ app/src/main/java/fiesta/hevs/ch/myapplication/TransportAltEndPointsInterface.java
@@ -0,0 +1,13 @@
+package fiesta.hevs.ch.myapplication;
+
+import java.util.List;
+
+import fiesta.hevs.ch.backend.transportAltApi.model.TransportAlt;
+
+
+/**
+ * Created by Chacha on 01.09.2016.
+ */
+public interface TransportAltEndpointsInterface {
+    void updateListView(List<TransportAlt> transportsAlt);
+}
