cf27b5824f101afaf9c04e0177579c671c44b9a4
benthieu <mathieu.b93@gmail.com>
2016-09-08 16:42:19 +0200

    fiesta: - communication update

---
+++ app/src/main/java/fiesta/hevs/ch/myapplication/CommunicationListActivity.java
@@ -0,0 +1,139 @@
+package fiesta.hevs.ch.myapplication;
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+import fiesta.hevs.ch.backend.communicationApi.model.Communication;
+import fiesta.hevs.ch.backend.transportApi.model.Transport;
+
+/**
+ * This class display some informations for a selected transport.
+ * @see TransportActivity
+ * @author Sylvain
+ */
+public class CommunicationListActivity extends AppCompatActivity implements ConversationListEndPointsInterface {
+    private ProgressDialog mDialog;
+
+    private Long transport_id;
+    private String android_id;
+    private String other_device_id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_communication_list);
+
+        Intent myIntent = getIntent(); // gets the previously created intent
+        transport_id = myIntent.getLongExtra("transport_id", 0);
+
+
+        android_id = Settings.Secure.getString(getBaseContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        other_device_id = myIntent.getStringExtra("other_device_id");
+
+        Button newComButton = (Button) findViewById(R.id.new_communication);
+        newComButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent;
+
+                myIntent = new Intent(CommunicationListActivity.this, CommunicationActivity.class);
+
+                myIntent.putExtra("transport_id", transport_id);
+                myIntent.putExtra("device_id_to", other_device_id);
+                myIntent.putExtra("device_id_from", android_id);
+                startActivity(myIntent);
+            }
+        });
+
+
+        mDialog = new ProgressDialog(CommunicationListActivity.this);
+        mDialog.setMessage("Charger les conversations...");
+        mDialog.setCancelable(false);
+        mDialog.show();
+
+        getConversations();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mDialog.show();
+        getConversations();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mDialog.dismiss();
+    }
+
+    public void getConversations() {
+        ConversationListEndPointsAsyncTask cLAT = new ConversationListEndPointsAsyncTask(this);
+        cLAT.setType(this.LIST);
+        cLAT.setDevice_id_1(android_id);
+        cLAT.setDevice_id_2(other_device_id);
+        if (transport_id != 0) {
+            cLAT.setFestival_transport_id(transport_id);
+        }
+        cLAT.execute();
+    }
+
+    @Override
+    public void updateListView(final List<Communication> communications) {
+        mDialog.hide();
+
+        if (communications == null) {
+            return;
+        }
+
+        final ListView listView = (ListView) findViewById(R.id.conversation_list);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_elem_conversation, R.id.textview_message, communications) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text_message = (TextView) view.findViewById(R.id.textview_message);
+                TextView text_name = (TextView) view.findViewById(R.id.textview_name);
+                LinearLayout message_layout = (LinearLayout) view.findViewById(R.id.layout_message);
+                LinearLayout message_parent = (LinearLayout) view.findViewById(R.id.layout_message_parent);
+                if (communications.get(position).getDeviceIdFrom().equals(android_id)) {
+                    message_parent.setGravity(Gravity.RIGHT);
+                    message_layout.setGravity(Gravity.RIGHT);
+                }
+
+                text_message.setText(communications.get(position).getMessage());
+                text_name.setText(communications.get(position).getName());
+
+                return view;
+            }
+        };
+        listView.setAdapter(adapter);
+        final Context context = this;
+    }
+
+    @Override
+    public void insertedCommunication() {
+
+    }
+}
+
--- app/src/main/java/fiesta/hevs/ch/myapplication/ConversationListActivity.java
+++ app/src/main/java/fiesta/hevs/ch/myapplication/ConversationListActivity.java
@@ -4,7 +4,9 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -15,11 +17,17 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.api.client.util.DateTime;
+
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import fiesta.hevs.ch.backend.communicationApi.model.Communication;
+import fiesta.hevs.ch.backend.festivalApi.model.Festival;
 
 /**
  * Created by Pascal on 01.09.2016.
@@ -28,11 +36,17 @@ public class ConversationListActivity extends AppCompatActivity implements Conve
     private ProgressDialog mDialog;
     private Long festival_id;
     private ImageButton backButton;
+    private String android_id;
+    private DateFormat dateFormat;
+    private DateFormat dateFormatInput;
+    private final List<comObj> comList = new ArrayList<comObj>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation_list);
+
+        android_id = Settings.Secure.getString(getBaseContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
         Intent myIntent = getIntent(); // gets the previously created intent
 
@@ -40,6 +54,9 @@ public class ConversationListActivity extends AppCompatActivity implements Conve
         mDialog.setMessage("Charger les conversations...");
         mDialog.setCancelable(false);
         mDialog.show();
+
+        dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        dateFormatInput = new SimpleDateFormat("yyyy-MM-dd");
 
         getConversations();
 
@@ -60,22 +77,70 @@ public class ConversationListActivity extends AppCompatActivity implements Conve
     }
 
     public void getConversations() {
-        new ConversationListEndPointsAsyncTask(this).execute();
+        ConversationListEndPointsAsyncTask cLAT = new ConversationListEndPointsAsyncTask(this);
+        cLAT.setType(this.LIST);
+        cLAT.setDevice_id_1(android_id);
+        cLAT.execute();
     }
 
 
     public void updateListView(final List<Communication> communications) {
         mDialog.hide();
+        if (communications == null || communications.size() == 0) {
+            return ;
+        }
+        comList.clear();
+        for (Communication c : communications) {
+            String otherdeviceid = c.getDeviceIdFrom();
+            if (c.getDeviceIdFrom().equals(android_id)) {
+                otherdeviceid = c.getDeviceIdTo();
+            }
+            boolean is_in_list = false;
+            for (comObj o : comList) {
+                if (o.other_device_id.equals(otherdeviceid)) {
+                    is_in_list = true;
+                    Date formatDate = null;
+                    try {
+                        formatDate = dateFormatInput.parse(c.getTimeSend().toString());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    if (formatDate != null) {
+                        if (o.last_date.before(formatDate)) {
+                            o.last_date = formatDate;
+                        }
+                    }
+                    o.msg_count++;
+                }
+            }
+            if (!is_in_list) {
+                comObj addList = new comObj();
+                Date formatDate = null;
+                try {
+                    formatDate = dateFormatInput.parse(c.getTimeSend().toString());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                addList.last_date = formatDate;
+                addList.name = c.getName();
+                addList.other_device_id = otherdeviceid;
+                comList.add(addList);
+            }
+        }
 
         final ListView listView = (ListView) findViewById(R.id.listView);
         setListViewHeightBasedOnChildren(listView);
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_elem_conversation, R.id.textview_1, communications) {
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_elem_communication, R.id.comm_name, comList) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
-                TextView text1 = (TextView) view.findViewById(R.id.textview_1);
+                TextView name = (TextView) view.findViewById(R.id.comm_name);
+                TextView msg_count = (TextView) view.findViewById(R.id.comm_message_count);
+                TextView date = (TextView) view.findViewById(R.id.comm_date);
 
-                text1.setText(communications.get(position).getName());
+                name.setText(comList.get(position).name);
+                msg_count.setText(comList.get(position).msg_count+" Messages");
+                date.setText(dateFormat.format(comList.get(position).last_date));
 
                 return view;
             }
@@ -88,9 +153,9 @@ public class ConversationListActivity extends AppCompatActivity implements Conve
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // show order edit/modify of this specific order
-                Intent myIntent = new Intent(ConversationListActivity.this, TransportActivity.class);
-                myIntent.putExtra("festival_id", communications.get(position).getId());
-                myIntent.putExtra("festival_name", communications.get(position).getName());
+                Intent myIntent = new Intent(ConversationListActivity.this, CommunicationListActivity.class);
+                //myIntent.putExtra("transport_id", communications.get(position).getId());
+                myIntent.putExtra("other_device_id", comList.get(position).other_device_id);
 
                 startActivity(myIntent);
             }
@@ -127,4 +192,10 @@ public class ConversationListActivity extends AppCompatActivity implements Conve
         listView.setLayoutParams(params);
     }
 
+    private class comObj {
+        public String other_device_id;
+        public Date last_date;
+        public String name;
+        public int msg_count;
+    }
 }
